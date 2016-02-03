2dad48161c5731aa8eeff94c09d55bbaa201907e
obiorahm <obiorahm@gmail.com>
2016-02-03 12:21:48 -0600

    use SMTP

--- contact.php
+++ contact.php
@@ -170,6 +170,18 @@ if ($action=="")    /* display the contact form */
     }  
 else                /* send the submitted data */ 
     { 
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
+
+$mail->Host       = "obiorahm@gmail.com"; // SMTP server example
+$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->Port       = 25;                    // set the SMTP port for the GMAIL server
+$mail->Username   = "obiorahm@gmail.com"; // SMTP account username example
+$mail->Password   = "uj/2004/ns/0229";   
+
     $name=$_REQUEST['name']; 
     $email=$_REQUEST['email']; 
     $message=$_REQUEST['message']; 
